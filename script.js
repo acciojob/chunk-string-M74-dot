49d347d0a54dd7950c53ec1acadcbbb5b8c2754e
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-07-25 13:18:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function stringChop(str, size) {
   // your code here
+	if (!str) {
+    return [];
+  }
+
+  const chunks = [];
+  const length = str.length;
+
+  // Loop through the string in steps of size
+  for (let i = 0; i < length; i += size) {
+    // Slice the string from current index to size characters ahead
+    chunks.push(str.slice(i, i + size));
+  }
+
+  return chunks;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size));
